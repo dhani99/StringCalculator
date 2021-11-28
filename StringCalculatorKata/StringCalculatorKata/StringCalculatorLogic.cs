8a670af725a03abe31c1a31245a25d18d6342dab
dhani99 <dhwani1999thakar@gmail.com>
2021-11-29 11:05:35 +1300

    updating changes

--- StringCalculatorKata/StringCalculatorKata/StringCalculatorLogic.cs
+++ StringCalculatorKata/StringCalculatorKata/StringCalculatorLogic.cs
@@ -1,10 +1,36 @@
+using System;
+
 namespace StringCalculatorKata
 {
     public class StringCalculatorLogic
     {
         public int Add(string aString)
         {
-            return 0;
+            if(aString == "")
+            {
+                return 0;
+            }
+            else if (aString.Length == 1)
+            {
+                var aStringAsNumber = int.Parse(aString);
+                return aStringAsNumber;
+            }
+            
+            var stringArray = aString.Split(",");
+            var total = 0; 
+            foreach (var number in stringArray)
+            {
+                total += int.Parse(number);
+
+            }
+            return total; 
+
+
+
+
+
+
+
         }
     }
 }
--- StringCalculatorKata/StringCalculatorTests/UnitTest1.cs
+++ StringCalculatorKata/StringCalculatorTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using StringCalculatorKata;
 using Xunit;
 
@@ -6,7 +7,7 @@ namespace StringCalculatorTests
     public class UnitTest1
     {
         [Fact]
-        public void CanTakeStringAndReturnANumber()
+        public void CanReturnAZeroWhenGivenAnEmptyString()
         {
             //arrange 
             var aString = "";
@@ -17,6 +18,57 @@ namespace StringCalculatorTests
             //assert
             Assert.Equal(0, result);
         }
+        
+        [Fact]
+        public void CanTakeStringAndReturnANumber()
+        {
+            //arrange 
+            var aString = "1";
+            var stringCalculatorLogic = new StringCalculatorLogic();
+            //act
+            var result = stringCalculatorLogic.Add(aString);
+            //assert
+            Assert.Equal(1, result);
+        }
+        
+        [Fact]
+        public void CanTake3AndReturnANumber()
+        {
+            //arrange 
+            var aString = "3";
+            var stringCalculatorLogic = new StringCalculatorLogic();
+            //act
+            var result = stringCalculatorLogic.Add(aString);
+            //assert
+            Assert.Equal(3, result);
+        }
+        [Fact]
+        public void CanTake2NumbersAndAddThemTogether()
+        {
+            //arrange 
+            var aString = "1,2";
+            var stringCalculatorLogic = new StringCalculatorLogic();
+            //act
+            var result = stringCalculatorLogic.Add(aString);
+            //assert
+            Assert.Equal(3, result);
+        }
+
+        [Theory]
+        [InlineData ("3,5",8)]
+        [InlineData ("1,2",3)]
+        [InlineData ("7,2",9)]
+        public void CanAdd2NumbersFromAString(string input, int expected)
+        {
+            //arrange 
+            var stringCalculatorLogic = new StringCalculatorLogic();
+            //act
+            var result = stringCalculatorLogic.Add(input);
+            //assert 
+            Assert.Equal(expected, result);
+            
+        }
+        
     }
     
 }
